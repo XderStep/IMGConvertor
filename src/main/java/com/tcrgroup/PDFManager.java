24c2a3a8c0dd66c23314787e9d6e6e7594b8efe1
XderStep <87845929+XderStep@users.noreply.github.com>
2024-02-13 14:58:08 +0200

    Update PDFManager.java

--- src/main/java/com/tcrgroup/PDFManager.java
+++ src/main/java/com/tcrgroup/PDFManager.java
@@ -1,2 +1,29 @@
-package com.tcrgroup;public class PDFManager {
+package com.tcrgroup;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class PDFManager {
+    public static void convertToPDF(File imageFile, File pdfFile) throws IOException {
+        try {
+            Document document = new Document();
+            FileOutputStream fos = new FileOutputStream(pdfFile);
+            PdfWriter.getInstance(document, fos);
+            document.open();
+
+            Image image = Image.getInstance(imageFile.getPath());
+            image.scaleToFit(document.getPageSize());
+            document.add(image);
+
+            document.close();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
